a67e8c4fdbf5165c4b890d864ff128c4a69d7d32
abhilash bhosle <abhilashbhosle10@gmail.com>
2021-08-03 21:27:09 +0530

    saga-app is ready

---
+++ saga-app/src/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+import {connect} from "react-redux";
+
+class App extends Component{
+  constructor(props){
+    super(props)
+  };
+  render(){
+    return(
+      <React.Fragment>
+        <h1>{this.props.bal}</h1>
+        <button onClick={this.props.withdraw}>withdraw</button>
+      </React.Fragment>
+    )
+  }
+}
+const receive=(state)=>{
+  console.log(state);
+  return{
+    bal:state.bal
+  }
+}
+const send=(dispatch)=>{
+  return{
+    withdraw:()=>{
+      dispatch({type:"WITHDRAW",value:1000})
+    }
+  }
+}
+export default connect (receive,send)(App);
