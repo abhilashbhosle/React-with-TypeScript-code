6af9e1703c18e640a274b8202dec3af9e73392d5
abhilash <abhilahbhosle10@gmail.com>
2021-08-25 21:02:42 +0530

    logical operations code

---
+++ logical-operations/src/Addition.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+class Addition extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            num1:"",
+            num2:"",
+            total:""
+        }
+    }
+        handlenum1=(event)=>{
+            this.setState({
+                num1:event.target.value
+            })
+        }
+            handlenum2=(event)=>{
+                this.setState({
+                    num2:event.target.value
+                })
+        }
+        addition=(event)=>{
+            this.setState({
+                total:parseInt(this.state.num1) + parseInt(this.state.num2)
+            })
+
+      
+        
+    }
+    render(){
+        return(
+            <div className="container">
+            <h1>Addition</h1>
+            <form>
+                <input type="text" value={this.state.num1} onChange={this.handlenum1}></input>{" "}
+                <input type="text" value={this.state.num2} onChange={this.handlenum2}></input><br></br><br></br>
+                <button onClick={this.addition}>Addition</button><br></br>
+                </form>
+                <h4>{this.state.total}</h4>
+            </div>        
+        )
+    }
+}
+export default Addition;
